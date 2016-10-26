aa7fc88169f6bb919f95e9f178640f5d9de6b402
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-10-26 11:32:36 +0300

    Basit hesap makinesi eklendi.

--- js/hm.js
+++ js/hm.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
     console.log("Hesap makinesi yüklendi.");
 });
+
+$("#topla").click(function () {
+    if (!isNaN($("#sayi1").val()) && !isNaN($("#sayi2").val())) {
+        var sonuc = parseFloat($("#sayi1").val());
+        sonuc += parseFloat($("#sayi2").val())
+        $("#sonuc").text("Sonuç: " + sonuc);
+    } else {
+        alert("Sayılar boş olamaz.");
+    }
+});
+
+$("#cikar").click(function () {
+    if (!isNaN($("#sayi1").val()) && !isNaN($("#sayi2").val())) {
+        var sonuc = parseFloat($("#sayi1").val());
+        sonuc -= parseFloat($("#sayi2").val())
+        $("#sonuc").text("Sonuç: " + sonuc);
+    } else {
+        alert("Sayılar boş olamaz.");
+    }
+});
+
+$("#carp").click(function () {
+    if (!isNaN($("#sayi1").val()) && !isNaN($("#sayi2").val())) {
+        var sonuc = parseFloat($("#sayi1").val());
+        sonuc *= parseFloat($("#sayi2").val())
+        $("#sonuc").text("Sonuç: " + sonuc);
+    } else {
+        alert("Sayılar boş olamaz.");
+    }
+});
+
+$("#bol").click(function () {
+    if (!isNaN($("#sayi1").val()) && !isNaN($("#sayi2").val())) {
+        var sonuc = parseFloat($("#sayi1").val());
+        sonuc /= parseFloat($("#sayi2").val())
+        $("#sonuc").text("Sonuç: " + sonuc);
+    } else {
+        alert("Sayılar boş olamaz.");
+    }
+});
